3a74db24299e430bd445f1844db63290fcddd5a0
Shtopor <a.45.mail.ru>
2023-07-23 19:26:45 +0500

    ver 1.1

---
+++ connection.php
@@ -0,0 +1,65 @@
+<?php
+class Bank{
+    private $conn;
+
+    public function __construct(){
+        $this->conn = new PDO("mysql:host=localhost;dbname=bank", "root", "190687");
+    }
+    
+    public function getCards(){
+        $sql="SELECT * FROM cards;";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function transact($summ, $card1, $card2){
+        if (empty($summ) || empty($card1) || empty($card2)){
+            echo "введите все данные";
+            return "";
+        }
+        try{
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->conn->beginTransaction();
+        $sql="UPDATE cards SET balance = balance + :summ WHERE number = :card2;";
+        $stmt= $this->conn->prepare($sql);
+        $stmt->bindValue(":summ", $summ);
+        $stmt->bindValue(":card2", $card2);
+        $stmt->execute();
+
+        $sql="UPDATE cards SET balance = balance - :summ WHERE number = :card1;";
+        $stmt= $this->conn->prepare($sql);
+        $stmt->bindValue(":summ", $summ);
+        $stmt->bindValue(":card1", $card1);
+        $stmt->execute(); 
+
+        $a = date('d.m.Y H:i:s');
+        $sql="INSERT INTO operations (card_append,descreption,card_pop,date) VALUES(:card2,:summ,:card1,:a);";
+        $stmt= $this->conn->prepare($sql);
+        $stmt->bindValue(":summ", $summ);
+        $stmt->bindValue(":card1", $card1);
+        $stmt->bindValue(":card2", $card2);
+        $stmt->bindValue(":a", $a);
+        $stmt->execute(); 
+
+        $this->conn->commit();
+    }
+    catch (Exception $e){
+        $this->conn->rollBack();
+        echo "ERRoR:" . $e->getMessage();
+    }
+    }
+
+    public function getAllTransactions(){
+        $sql="SELECT * FROM operations;";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        return $stmt;
+    }
+
+}
+
+    
+
+
+?>
---
+++ view.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="view.php" method="POST">
+        <input type="number" name="summ" placeholder="сумма"></br>
+        <input type="text" name="card1" placeholder="карта списания"></br>
+        <input type="text" name="card2" placeholder="карта зачисления"></br>
+        <input type="submit" name="transact" value="TRANSACT">
+        <input type="submit" name="show_all_transactions" value="Show ALL Transactions">
+    </form>
+</body>
+</html>
+
+<?php
+
+include "connection.php";
+
+$res = new Bank();
+$data = $res->getCards();
+foreach ($data as $row) {
+    echo $row["number"] . " ";
+    echo $row["balance"] . " ";
+    echo "рублей" . "</br>";        
+}
+
+if(isset($_POST["transact"])){
+    $summ = $_POST["summ"];
+    $card1 = $_POST["card1"];
+    $card2 = $_POST["card2"];
+    $res->transact($summ, $card1, $card2);
+    header("Location:http://localhost/php_files/Bank/view.php");
+}
+
+if(isset($_POST["show_all_transactions"])){
+    $data = $res->getAllTransactions();
+    foreach ($data as $row){
+        echo "ПЕРЕВОД" . " ";
+        echo "отправитель:" . $row["card_append"] . " ";
+        echo "сумма:" . $row["descreption"] . " ";
+        echo "получатель:" . $row["card_pop"] . " ";
+        echo "дата:" . $row["date"] . " " . "</br>";
+    }
+}
+?>
